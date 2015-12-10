5cce2c77b559ca2c43dad87fba81bad2fe4506ca
Dong Ha. Park <luncliff@gmail.com>
2015-12-11 01:17:44 +0900

    Create Listing_4_3.cpp
    Building a thread-safe queue with condition variables
    p.72

---
+++ chapter-4/Listing_4_3.cpp
@@ -0,0 +1,24 @@
+// [Listing 4.3]
+// The interface of your threadsafe_queue
+
+#include <memory>
+
+template<typename T>
+class threadsafe_queue
+{
+public:
+	threadsafe_queue();
+	threadsafe_queue(const threadsafe_queue&);
+	threadsafe_queue& operator=(
+		const threadsafe_queue&) = delete;
+		
+	void push(T new_value);
+	
+	bool try_pop(T& value);
+	std::shared_ptr<T> try_pop();
+	
+	void wait_and_pop(T& value);
+	std::shared_ptr<T> wait_and_pop();
+	
+	bool empty() const;
+};
